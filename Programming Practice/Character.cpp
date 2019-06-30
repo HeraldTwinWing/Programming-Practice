14f3ea7c8c4cf46d40f6a4fe70ed4a5293bbf3f2
Yvonnaaaa <49388751+Yvonnaaaa@users.noreply.github.com>
2019-06-30 16:53:38 +0800

    wotaicai

--- Programming Practice/Character.cpp
+++ Programming Practice/Character.cpp
@@ -1,1 +1,187 @@
 #include "Character.h"
+#include<cmath>
+
+Character::Character()
+{
+
+	this->Character_position.x = position.x + 20;
+	this->Character_position.y = position.y + 20;
+
+	lastMove = SDL_GetTicks();
+};
+void Character::spaw()
+{
+	SDL_RenderCopy(window->getRender(), texture, nullptr, &position);
+}
+
+
+void Character::move(Map &NowMap)
+{
+	int i = (int)position.y / 40;
+	int j = (int)position.x / 40;
+	double timeLenth = 0.001*(SDL_GetTicks() - lastMove);
+	if (moving[0] && position.y >= 0 && (i-1)>=0 && NowMap.loadMap[i-1][j] == EMPYT)
+	{
+		moveTemp.second -= speed * timeLenth;
+	}
+	if (moving[1] && position.y <= 480 && (i+1))<=480 NowMap.loadMap[i+1][j] == EMPYT)
+	{
+		moveTemp.second += speed * timeLenth;
+	}
+	if (moving[2] && position.x >= 0 && (j-1)>=0 && NowMap.loadMap[i][j-1] == EMPYT)
+	{
+		moveTemp.first -= speed * timeLenth;
+	}
+	if (moving[3] && position.x <= 560 && (j+1)<=560 NowMap.loadMap[i][j+1] == EMPYT)
+	{
+		moveTemp.first += speed * timeLenth;
+	}
+
+	if (std::abs(moveTemp.first) > 1)
+	{
+		position.x += (int)std::trunc((float)moveTemp.first);
+		moveTemp.first = std::trunc((float)moveTemp.first);
+	}
+	if (std::abs(moveTemp.second) > 1)
+	{
+		position.y += (int)std::trunc((float)moveTemp.second);
+		moveTemp.second = std::trunc((float)moveTemp.first);
+	}
+
+	lastMove = SDL_GetTicks();
+}
+
+//bool Character::Judge(Map& Nmap)
+//{
+//	int i = (int)position.x / 40 ;
+//	int j = (int)position.y / 40;
+//	if (Nmap.map[i][j] == DANGER)
+//	{
+//		this->damage_amount = true;
+//	}
+//}
+
+bool Character::damage(bool damage_amount)
+{
+	if (damage_amount == true)
+	{
+		return true;
+	}
+}
+
+void Character::refresh()
+{
+	move();
+
+	//还有炸弹的位置
+
+	showImage();
+}
+
+void Character::showImage()
+{
+	SDL_RenderCopy(window->getRender(), texture, nullptr, &position);
+}
+
+void Character::keyDownEvent1(SDL_Keycode sym)
+{
+	switch (sym)
+	{
+	case SDLK_w:
+		moving[0] = true;
+		moving[1] = false;
+		break;
+	case SDLK_s:
+		moving[1] = true;
+		moving[0] = false;
+		break;
+	case SDLK_a:
+		moving[2] = true;
+		moving[3] = false;
+		break;
+	case SDLK_d:
+		moving[3] = true;
+		moving[2] = false;
+		break;
+	case SDLK_SPACE:
+		set = true;
+	default:
+		break;
+	}
+}
+
+void Character::keyUpEvent1(SDL_Keycode sym)
+{
+	switch (sym)
+	{
+	case SDLK_w:
+		moving[0] = false;
+		break;
+	case SDLK_s:
+		moving[1] = false;
+		break;
+	case SDLK_a:
+		moving[2] = false;
+		break;
+	case SDLK_d:
+		moving[3] = false;
+		break;
+	case SDLK_SPACE:
+		set = false;
+	default:
+		break;
+	}
+}
+void Character::keyDownEvent2(SDL_Keycode sym)
+{
+	switch (sym)
+	{
+	case SDLK_UP:
+		moving[0] = true;
+		moving[1] = false;
+		break;
+	case SDLK_DOWN:
+		moving[1] = true;
+		moving[0] = false;
+		break;
+	case SDLK_LEFT:
+		moving[2] = true;
+		moving[3] = false;
+		break;
+	case SDLK_RIGHT:
+		moving[3] = true;
+		moving[2] = false;
+		break;
+	case SDLK_KP_ENTER:
+		set = true;
+	default:
+		break;
+	}
+}
+
+void Character::keyUpEvent2(SDL_Keycode sym)
+{
+	switch (sym)
+	{
+	case SDLK_UP:
+		moving[0] = false;
+		break;
+	case SDLK_DOWN:
+		moving[1] = false;
+		break;
+	case SDLK_LEFT:
+		moving[2] = false;
+		break;
+	case SDLK_RIGHT:
+		moving[3] = false;
+		break;
+	case SDLK_KP_ENTER:
+		set = false;
+	default:
+		break;
+	}
+}
+
+Character::Character()
+{
+};
--- Programming Practice/Character.h
+++ Programming Practice/Character.h
@@ -1,10 +1,85 @@
 #pragma once
+#include<SDL.h>
+#include<string>
+#include<utility>
+#include<deque>
+#include<memory>
+#include<cmath>
+#include"SDL_keyboard.h"
+#include"Window.h"
+#include"GameEvent.h"
+#include"bomb.h"
+#include"Map.h"
 class Character
 {
 private:
-    int speed;
-    int moveTemp;   //保存位移的小数部分
+	//移动速度，每秒移动的像素（每秒40个像素）
+    int speed;        
 
+	//上下左右移动状态
+	bool moving[4] = { false,false,false,false };
+	double lastMove;
+	//保存左右、上下位移的小数部分
+    std::pair<double,double> moveTemp;  
+
+	//炸弹
+
+	bool set;
+
+	//击中判定
+
+	//bool damage_amount;
+
+	//坐标与大小
+	SDL_Point Character_position;  //人物的位置
+	SDL_Texture *texture = nullptr;
+	SDL_Rect position;
+
+	//人物所在渲染层
+	Window *window = nullptr;
+public:
+	Character();
+
+	//生成单位
+	virtual void spaw();
+
+	/*控制人物的移动
+	 *该方法输入移动方向
+	 *根据速度控制人物的坐标向该方向改变
+	 * */
+	virtual void move(Map &NowMap);
+
+	////判断是否在炸弹范围内
+	//bool Judge(Map& map);
+
+	//被炸弹炸死
+	//true则返回true，否则返回false
+	virtual bool damage(bool damage_amount);
+
+	/*拾取道具
+	 *拾取道具后改变当前属性
+	 * */
+	void change_nature();                     //缺少道具？
+
+	/*放置炸弹
+	 **/
+	virtual void setbomb();                   //?
+
+	//刷新人物状态
+	virtual void refresh();
+	virtual void showImage();
+	
+	//处理与玩加人物相关事件
+	void keyDownEvent1(SDL_Keycode sym);
+	void keyDownEvent2(SDL_Keycode sym);
+	void keyUpEvent1(SDL_Keycode sym);
+	void keyUpEvent2(SDL_Keycode sym);
+	bool if_setBomb()
+	{
+		return set;
+	}
+
+	//virtual bool if_not_
     
 };
 
