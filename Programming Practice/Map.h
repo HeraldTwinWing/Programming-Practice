1e99d8277238e09f874291542aa6460ab73a1456
钰香 郭 <2636791876@qq.com>
2019-06-30 09:13:01 +0800

    QAQ

--- Programming Practice/Map.h
+++ Programming Practice/Map.h
@@ -2,20 +2,22 @@
 
 enum BlockTypeEnum
 {
-     DESTRUCTIBLE, UNDESTRUCTIBLE, EMPYT
+     DESTRUCTIBLE, UNDESTRUCTIBLE, EMPYT,DANGER,BOMB//DAGER 为爆炸波及范围，BOMB为炸弹位置
 };
 
 class Map
 {
 private:
-    BlockTypeEnum map[12][12];
+    BlockTypeEnum map[13][15];
 public:
     Map();
+	Map(int a);
+	static char MapDate[13][15];
 
     //从文件加载地图
     void loadMap();
 
     //刷新地图图像
-    void refresh();
+    void refresh(int x,int y,bool boom);
 };
 
